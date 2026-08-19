ed413ef2fb060a40ef51fb4d504ae5e55bb47f11
cabrah13 <charlottemabraham32@gmail.com>
2026-08-19 11:58:41 -0400

    finished Member and MemberDao files

---
+++ src/main/java/com/mycompany/app/Member.java
@@ -0,0 +1,43 @@
+package com.mycompany.app;
+
+public class Member {
+    private int id;
+    private String name;
+    private String email;
+
+    public Member(int id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    //creating a new member to insert, postgres will generate an ID once you insert it
+    public Member(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id; 
+    }
+
+    public void setId(int idInput) {
+        this.id = idInput;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setTitle(String nameInput) {
+        this.name = nameInput;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String emailInput) {
+        this.email = emailInput;
+    }
+}
---
+++ src/main/java/com/mycompany/app/MemberDao.java
@@ -0,0 +1,148 @@
+package com.mycompany.app;
+
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MemberDao {
+    public static void addMember(Member member) {
+        String sql = "INSERT INTO members (name, email) VALUES (?, ?)";
+
+        //PreparedStatement features of the JDBC API used to execute parameterized SQL queries securely and efficiently
+        try(
+            Connection conn = DBConnection.getConnection(); 
+            PreparedStatement stmt = conn.prepareStatement(sql)) 
+        {
+            stmt.setString(1, member.getName());
+            stmt.setString(2, member.getEmail());
+
+            int rows = stmt.executeUpdate();
+            System.out.println(rows + " member(s) inserted successfully");
+        } catch(SQLException | IOException e) {
+            System.out.println("Failed to add member(s): " + e.getMessage());
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static List<Member> getAllMembers() {
+        String sql = "SELECT * FROM members";
+        List<Member> members = new ArrayList<Member>();
+
+        //ResultSet in Java is an object that holds the data returned from a database after executing a SQL query (SELECT)
+        //.next() method advances cursor through first row and subsequent rows, method returns true if a row exists and false otherwise, ideal for while loop
+        //use getter to extract column data by using column name
+        try(
+            Connection conn = DBConnection.getConnection();
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(sql)) {
+                while(rs.next()) {
+                    Member m = new Member(rs.getInt("id"), rs.getString("name"), rs.getString("email"));
+                    members.add(m);
+                }
+            } catch (SQLException| IOException e) {
+                System.out.println("Failed to load all books: " + e.getMessage());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        return members;
+    }
+
+    public static Member getMemberId(int id) {
+        String sql = "SELECT * FROM members WHERE id = ?";
+        Member m = null;
+
+        //Prepared Statement because it has parameters
+        try(
+            Connection conn = DBConnection.getConnection(); 
+            PreparedStatement stmt = conn.prepareStatement(sql)) 
+        {
+            stmt.setInt(1, id);
+
+            try(ResultSet rs = stmt.executeQuery()) {
+                if(rs.next() == true) {
+                    m = new Member(rs.getInt("id"), rs.getString("name"), rs.getString("email"));
+                }
+            }
+
+        } catch(SQLException| IOException e) {
+            System.out.println("Failed to load book: " + e.getMessage());
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+
+        return m;
+    }
+
+    public static List<Member> searchByName(String name) {
+        String sql = "SELECT * FROM members WHERE name ILIKE ?";
+        List<Member> searchedMembers = new ArrayList<Member>();
+
+        try(
+            Connection conn = DBConnection.getConnection(); 
+            PreparedStatement stmt = conn.prepareStatement(sql)) 
+        {
+            stmt.setString(1, "%" + name + "%");
+
+            try(ResultSet rs = stmt.executeQuery()) {
+                while(rs.next()) {
+                    Member m = new Member(rs.getInt("id"), rs.getString("name"), rs.getString("email"));
+                    searchedMembers.add(m);
+                }
+            }
+        } catch(SQLException| IOException e) {
+            System.out.println("Failed to search: " + e.getMessage());
+
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+
+        return searchedMembers;
+    }
+
+    public static void updateMember(Member member) {
+        String sql = "UPDATE members SET name = ?, email = ? WHERE id = ?";
+
+        try(
+            Connection conn = DBConnection.getConnection(); 
+            PreparedStatement stmt = conn.prepareStatement(sql)) 
+        {
+            stmt.setString(1, member.getName());
+            stmt.setString(2, member.getEmail());
+            stmt.setInt(3, member.getId());
+
+            int rows = stmt.executeUpdate();
+            System.out.println(rows + " member(s) updated successfully");
+        } catch(SQLException | IOException e) {
+            System.out.println("Failed to update member(s): " + e.getMessage());
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void deleteMember(int id) {
+        String sql = "DELETE FROM members WHERE id = ?";
+
+        try(
+            Connection conn = DBConnection.getConnection(); 
+            PreparedStatement stmt = conn.prepareStatement(sql)) 
+        {
+            stmt.setInt(1, id);
+
+            int rows = stmt.executeUpdate();
+            System.out.println(rows + " memeber(s) deleted successfully!");
+
+        } catch(SQLException | IOException e) {
+            System.out.println("Failed to delete member: " + e.getMessage());
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
