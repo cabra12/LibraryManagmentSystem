98d1679b3906cf357f0714e82d6cfa24d4cb8502
cabrah13 <charlottemabraham32@gmail.com>
2026-09-15 20:00:23 -0400

    Continuing AuthHelper tests, tested the following methods: getEmailInput, checkPassword, retrievePassword, and getUsernameInput,

--- src/test/java/com/mycompany/app/AuthHelperTest.java
+++ src/test/java/com/mycompany/app/AuthHelperTest.java
@@ -30,7 +30,44 @@ public class AuthHelperTest {
             assertEquals("Sara Smith", result);
         }
     }
-    
+
+    @Nested
+    @DisplayName("getEmailInput tests")
+    class GetEmailInputTests {
+
+        @Test
+        @DisplayName("valid email entered on first try")
+        public void getEmailInput_validEmail() {
+            Scanner fakeInput = new Scanner("charlotte@gmail.com\n");
+            String result = AuthHelper.getEmailInput(fakeInput);
+            assertEquals("charlotte@gmail.com", result);
+        }
+
+        @Test
+        @DisplayName("invalid email input with no @ symbol and then valid email given")
+        public void getEmailInput_noAtSymbol() {
+            Scanner fakeInput = new Scanner("sara\nsara@gmail.com");
+            String result = AuthHelper.getEmailInput(fakeInput);
+            assertEquals("sara@gmail.com", result);
+        }
+
+        @Test
+        @DisplayName("invalid empty email input and then valid email given")
+        public void getEmailInput_emptyInput() {
+            Scanner fakeInput = new Scanner("\nkatherine@gmail.com");
+            String result = AuthHelper.getEmailInput(fakeInput);
+            assertEquals("katherine@gmail.com", result);
+        }
+
+        @Test
+        @DisplayName("invalid email with no @ symbol, invalid empty email input,then valid email given")
+        public void getEmailInput_emptyAndNoAtSymbolInput() {
+            Scanner fakeInput = new Scanner("rachel\n\nrachel@gmail.com");
+            String result = AuthHelper.getEmailInput(fakeInput);
+            assertEquals("rachel@gmail.com", result);
+        }
+    }
+
     @Nested
     @DisplayName("makeNewPassword tests")
     class MakeNewPasswordTests {
@@ -75,6 +112,71 @@ public class AuthHelperTest {
             Scanner fakeInput = new Scanner("greenflowers4me\ngreen\ngreenflower\ngreenflowers4\ngreenflowers4me");
             String result = AuthHelper.makeNewPassword(fakeInput);
             assertTrue(BCrypt.checkpw("greenflowers4me", result));
+        }
+    }
+
+    @Nested
+    @DisplayName("checkPassword tests")
+    class CheckPasswordTests {
+        @Test 
+        @DisplayName("matched passwords checked, should return true")
+        public void checkPassword_matchedPasswords() {
+            String password = "pass123!$p";
+            String hashedPw = BCrypt.hashpw(password, BCrypt.gensalt(12));
+            boolean output = AuthHelper.checkPassword(password, hashedPw);
+            assertTrue(output);
+        }
+
+        @Test
+        @DisplayName("mismatched passwords checked, should return false")
+        public void checkPassword_mismatchedPasswords() {
+            String actualPassword = "fortune150";
+            String mistypedPassword = "for150";
+
+            String hashedPw = BCrypt.hashpw(actualPassword, BCrypt.gensalt(12));
+            boolean output = AuthHelper.checkPassword(mistypedPassword, hashedPw);
+            assertFalse(output);
+        }
+    }
+
+
+    @Nested
+    @DisplayName("retrievePassword tests")
+    class RetrievePasswordTests {
+        @Test 
+        @DisplayName("valid password entered on first try")
+        public void retrievePassword_nonEmptyPassword() {
+            Scanner fakeInput = new Scanner("password123\n");
+            String result = AuthHelper.retrievePassword(fakeInput);
+            assertEquals("password123", result);
+        }
+
+        @Test
+        @DisplayName("empty input first, then a valid password input")
+        public void retrievePassword_emptyPassword() {
+            Scanner fakeInput = new Scanner("\nredhot&dangerous\n");
+            String result = AuthHelper.retrievePassword(fakeInput);
+            assertEquals("redhot&dangerous", result);
+        }
+    }
+
+    @Nested
+    @DisplayName("getUsernameInput tests")
+    class GetUsernameInputTests {
+        @Test 
+        @DisplayName("valid username entered on first try")
+        public void getUsernameInput_nonEmptyUsername() {
+            Scanner fakeInput = new Scanner("char12\n");
+            String result = AuthHelper.getUsernameInput(fakeInput);
+            assertEquals("char12", result);
+        }
+
+        @Test
+        @DisplayName("empty input first, then a valid username input")
+        public void getUsernameInput_emptyUsername() {
+            Scanner fakeInput = new Scanner("\nwillow1\n");
+            String result = AuthHelper.getUsernameInput(fakeInput);
+            assertEquals("willow1", result);
         }
     }
 
